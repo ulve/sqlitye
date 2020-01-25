e80c2e84190745482039f51e343bafae6ad3849f
Olov Johansson <olov.johansson@protonmail.com>
2020-01-25 23:55:50 +0100

    Nu fungerar det med en liten lätt webbservice

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,44 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Main where
 
-import Lib
+import Control.Concurrent
+import Control.Monad.IO.Class
+import Database.SQLite.Simple
+import Network.Wai.Handler.Warp
+import Servant
+
+type Message = String
+
+type API = ReqBody '[PlainText] Message :> Post '[JSON] NoContent :<|> Get '[JSON] [Message]
+
+api :: Proxy API
+api = Proxy
+
+initDB :: FilePath -> IO ()
+initDB dbFile = withConnection dbFile $ \conn -> execute_ conn "CREATE TABLE IF NOT EXISTS messages (msg text not null)"
+
+server :: FilePath -> Server API
+server dbFile = postMessage :<|> getMessages
+  where postMessage :: Message -> Handler NoContent
+        postMessage msg = do 
+          liftIO . withConnection dbFile $ \conn ->
+            execute conn "INSERT INTO messages VALUES (?)" (Only msg)
+          return NoContent
+
+        getMessages :: Handler [Message]
+        getMessages = fmap (map fromOnly) . liftIO $
+          withConnection dbFile $ \conn -> query_ conn "SELECT msg FROM messages"
+
+
+runApp :: FilePath -> IO ()
+runApp dbFile = run 8080 (serve api $ server dbFile)
+
 
 main :: IO ()
-main = runPersonExample 
+main = do
+  let dbFile = "test2.db"
+  initDB dbFile
+  runApp dbFile 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,39 +1,7 @@
-{-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-  ( runPersonExample 
+  ( fifi 
   ) where
 
-import           Control.Monad (mapM_)
-import           Data.Int (Int64)
-import           Data.Text (Text)
-import qualified Data.Text as T
-import           Database.SQLite.Simple
-import           Database.SQLite.Simple.FromRow
-import           Database.SQLite.Simple.FromField
-import           Database.SQLite.Simple.ToField
-import           Database.SQLite.Simple.Internal
-import           Database.SQLite.Simple.Ok
-
-data Person =
-  Person 
-    { personId   :: Int64
-    , personName :: Text
-    , personAge  :: Text
-    } deriving (Eq,Read,Show)
-
-instance FromRow Person where
-    fromRow = Person <$> field <*> field <*> field
-
-instance ToRow Person where
-  toRow (Person _pId pName pAge) = toRow (pAge, pName)
-
-runPersonExample :: IO ()
-runPersonExample = do
-  conn <- open "test.db"
-  execute_ conn "CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age TEXT)"
-  execute conn "INSERT INTO people (name, age) VALUES (?,?)" (Person 0 "Justina" "15")
-  execute conn "INSERT INTO people (name, age) VALUES (?,?)" (Person 0 "Jordi" "11")
-  people <- query_ conn "SELECT id, name, age from people" :: IO [Person]
-  close conn
-  print people
+fifi :: String
+fifi = "Hej"
